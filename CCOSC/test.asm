2ac5f01e02962edee8b81115b33ec830d9db6868
BakedPotato <90150681+bpotato198@users.noreply.github.com>
2025-04-07 09:03:00 +0200

    Update test.asm

--- CCOSC/test.asm
+++ CCOSC/test.asm
@@ -1,4 +1,8 @@
 .global boot
 .global init
+.global sysinit
 
 # soon to be implemented a init system
+# currently the entire project is unbuildable
+# due to not properly configured makefiles and CMakeLists
+# new builds and sources will have new and rewritten makefiles and CMakeLists
