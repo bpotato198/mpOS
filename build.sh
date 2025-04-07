e13126c0053e555d263f4027d18793eac7037d4d
BakedPotato <90150681+bpotato198@users.noreply.github.com>
2025-04-07 10:47:48 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 # build script for CCOS uses CMake
 
-cd CCOSC && mkdir build && cd build && echo 'building makefiles...' && cmake ..  
+cd mpos && cd src && mkdir build && cd build && echo 'building makefiles...' && cmake ..  
