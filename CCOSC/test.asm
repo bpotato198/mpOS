64fad57370b0d3504a92430d2d2dfac7b861d9ba
BakedPotato <90150681+bpotato198@users.noreply.github.com>
2025-04-07 09:39:16 +0200

    Update test.asm

--- CCOSC/test.asm
+++ CCOSC/test.asm
@@ -1,8 +1,12 @@
 .global boot
 .global init
-.global sysinit
+.global userlandinit
+
 
 # soon to be implemented a init system
 # currently the entire project is unbuildable
 # due to not properly configured makefiles and CMakeLists
 # new builds and sources will have new and rewritten makefiles and CMakeLists
+# boot is for booting the system itself
+# init is a POST-Like system for checking if everything is alright and no files are missing
+# userlandinit starts the shell itself
